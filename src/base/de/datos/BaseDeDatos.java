b1a08178c185079f47986acc3847fa60a66f030c
FranLL1DAW <FranLL1DAW@FranLL1DAW-PC>
2016-11-22 13:27:47 +0100

    aplication

---
+++ src/base/de/datos/BaseDeDatos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base.de.datos;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author FranLL1DAW
+ */
+public class BaseDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+       indice ind = new indice();
+       ind.setVisible(true);
+               
+    }
+    
+}
